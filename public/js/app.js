d37be33814d5b7a087fe017a2c95c0c95d6d7bb6
leo melgar <leo.melgar33@gmail.com>
2015-09-01 21:57:06 -0300

    adding entrance for product

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles','mean.clients']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles','mean.clients','mean.products']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
 angular.module('mean.clients', []);
+angular.module('mean.products', []);
